920fa4ec7b1dc6ebcf67124e390dc71c8795c3c0
Sam Acquaviva <samacqua@gmail.com>
2023-04-24 18:34:26 -0400

    filled out basic training script.

--- train.py
+++ train.py
@@ -1,7 +1,31 @@
 """Trains the models."""
 
-def train():
-    raise NotImplementedError
+import torch
+from torch.utils.tensorboard import SummaryWriter
+
+from tqdm import tqdm
+import yaml
+
+
+def train(model: torch.nn.Module, train_loader: torch.utils.data.DataLoader, optimizer: torch.optim.Optimizer, 
+          criterion: torch.nn.Module, device: torch.device, epochs: int = 1, log_dir: str = None):
+    """Trains a model."""
+    model.train()
+    writer = SummaryWriter(log_dir=log_dir)
+
+    for batch_idx, (data, target) in enumerate(tqdm(train_loader)):
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = criterion(output, target)
+        loss.backward()
+        optimizer.step()
+
+        # if batch_idx == 50:
+        #     break
+
+        writer.add_scalar('train/loss', loss.item(), batch_idx)
+
 
 if __name__ == '__main__':
     pass
