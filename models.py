3ead4b704e97a70f8ba7e0de864d399bab6fd167
Sam Acquaviva <samacqua@gmail.com>
2023-04-24 18:35:12 -0400

    added (untested / probably wrong) ability to change between gabor-constrained and unconstrained

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 """Load the base models."""
 
+import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
@@ -11,12 +12,14 @@ class CNN(nn.Module):
         
     def __init__(self, is_gabornet: bool = False):
         super().__init__()
-        self.conv1 = GaborConv2d if is_gabornet else nn.Conv2d(3, 6, 5)
+        self.conv1 = GaborConv2d(3, 6, 5) if is_gabornet else nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
+
+        self.is_gabornet = is_gabornet
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
@@ -26,6 +29,26 @@ class CNN(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
+    
+    def change_constraint(self, gabor_constrained: bool):
+        """Makes the first layer a GaborNet or standard convolutional layer."""
+
+        # Return if nothing needs to be changed.
+        if (gabor_constrained and self.is_gabornet) or (not gabor_constrained and not self.is_gabornet):
+            return
+        
+        # Get current parameters.
+        old_params = self.conv1.state_dict()
+
+        # TODO: handle bias.
+        # Create a new layer and use old weights.
+        new_layer_type = GaborConv2d if gabor_constrained else nn.Conv2d
+        self.conv1 = new_layer_type(3, 6, 5)
+        new_params = self.conv1.state_dict()
+        new_params['weight'] = old_params['weight']
+        self.conv1.load_state_dict(new_params)
+
+        self.is_gabornet = gabor_constrained
 
 
 if __name__ == "__main__":
