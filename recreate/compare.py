21ee0f42412ac2d585efbf639e38306b64dbbce9
Sam Acquaviva <samacqua@gmail.com>
2023-05-11 12:21:03 -0400

    comparison across multiple runs / error bars

--- recreate/compare.py
+++ recreate/compare.py
@@ -5,6 +5,7 @@ import os
 import argparse
 import torch
 import json
+import numpy as np
 
 from torch.utils.data import DataLoader
 
@@ -12,7 +13,8 @@ from .analyze import load_models, calc_accuracies_full
 from .dogs_cats import DogCatNNSanity, DogCatNet, load_dataset
 
 
-def compare_models(models_train: dict[str, dict[int, float]], models_test: dict[str, dict[int, float]], save_dir: str):
+def compare_models(models_train: dict[str, dict[int, float | tuple[float, float]]], 
+                   models_test: dict[str, dict[int, float | tuple[float, float]]], save_dir: str):
     """Makes plots to compare the performance of many models."""
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 5))
@@ -20,7 +22,9 @@ def compare_models(models_train: dict[str, dict[int, float]], models_test: dict[
     # Plot the training accuracy.
     for model_name, model_accs in models_train.items():
         train_x, train_y = zip(*sorted(model_accs.items()))
-        ax1.plot(train_x, train_y, label=model_name)
+        train_y, std_err = zip(*train_y)
+        p = ax1.plot(train_x, train_y, label=model_name)
+        ax1.errorbar(train_x, train_y, yerr=std_err, fmt="o", capsize=5, color=p[0].get_color())
 
     ax1.set_xlabel("Epoch")
     ax1.set_ylabel("Training Accuracy")
@@ -30,7 +34,9 @@ def compare_models(models_train: dict[str, dict[int, float]], models_test: dict[
     # Plot the test accuracy.
     for model_name, model_accs in models_test.items():
         test_x, test_y = zip(*sorted(model_accs.items()))
-        ax2.plot(test_x, test_y, label=model_name)
+        test_y, std_err = zip(*test_y)
+        p = ax2.plot(test_x, test_y, label=model_name)
+        ax2.errorbar(test_x, test_y, yerr=std_err, fmt="o", capsize=5, color=p[0].get_color())
 
     ax2.set_xlabel("Epoch")
     ax2.set_ylabel("Test Accuracy")
@@ -59,8 +65,6 @@ def main():
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--seed", type=int, help="The random seed of the experiment to analyze.")
-
     parser.add_argument("--model", type=str, default="DogCatNet", help="The base model to use for the CNNs.",
                         choices=["DogCatNet", "DogCatNNSanity"])
     
@@ -76,14 +80,22 @@ def main():
     base_model = globals()[args.model]
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+    # Load the comparison json file.
     with open("recreate/compare.json", "r") as f:
-        model_dirs = json.load(f)
+        model_dirs_ = json.load(f)
+
+        # Convert seed string to int.
+        model_dirs = {}
+        for model_name in model_dirs_:
+            for seed_str, dir in model_dirs_[model_name].items():
+                model_dirs.setdefault(model_name, {})[int(seed_str)] = dir
 
     # Ensure that models are comparable + set dataset path.
     img_size = None
     dataset_dir = args.dataset_dir
-    for model_name, model_dir in model_dirs.items():
-        exp_path = get_exp_path(model_dir)
+    for model_name, model_dir_by_seed in model_dirs.items():
+        exp_path = get_exp_path(list(model_dir_by_seed.values())[0])    # All have the same experiment info.
+
         exp_args_path = os.path.join(exp_path, "args.json")
         with open(exp_args_path, "r") as f:
             exp_args = json.load(f)
@@ -99,51 +111,84 @@ def main():
             else:
                 assert dataset_dir == exp_args['dataset_dir'], "Dataset directories are not the same."
 
-    epochs = [epoch - 1 for epoch in args.epochs]
+    epochs = [epoch - 1 for epoch in args.epochs]   # 0-index epochs.
 
     # Load the models.
-    models = {}
-    for model_name, model_dir in model_dirs.items():
-        accuracy_dir = get_acc_path(model_dir)
-        models[model_name] = {
-            "checkpoints": load_models(base_model, epochs, model_dir, device, calc_weights=False),
-            "save_dir": accuracy_dir
-            }
+    models_by_seed = {}
+    for model_name, model_dir_by_seed in model_dirs.items():
+        for seed, model_dir in model_dir_by_seed.items():
+            accuracy_dir = get_acc_path(model_dir)
+            models_by_seed.setdefault(seed, {})[model_name] = {
+                "checkpoints": load_models(base_model, epochs, model_dir, device, calc_weights=False),
+                "save_dir": accuracy_dir
+                }
+
+    train_accs_by_seed = {}
+    test_accs_by_seed = {}
+    for seed, models in models_by_seed.items():
+
+        # Load the testset.
+        print(f"Loading dataset with seed={seed}...")
+        torch.manual_seed(seed)
+        train_set, test_set = load_dataset(dataset_dir, img_size=(img_size, img_size))
+
+        # Split the trainset into train and test.
+        train_set, test_set1 = torch.utils.data.random_split(
+            train_set, [int(len(train_set) * 0.7), int(len(train_set) * 0.3)]
+        )
+
+        # Limit the dataset size to make the computations faster.
+        N_train = args.N if args.N else len(train_set)
+        N_val = args.N if args.N else len(test_set1)
+        N_test = args.N if args.N else len(test_set)
+        train_set, _ = torch.utils.data.random_split(train_set, [N_train, len(train_set) - N_train])
+        val_set, _ = torch.utils.data.random_split(test_set1, [N_val, len(test_set1) - N_val])
+        test_set = torch.utils.data.random_split(test_set, [N_test, len(test_set) - N_test])
+        # test_set = val_set      # Pretty sure the original papers "test set" is actually a validation set.
+
+        batch_size = 64
+        train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=0)
+        test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=0)
         
-    # Load the testset.
-    print("Loading dataset...")
-    torch.manual_seed(args.seed)
-    train_set, test_set = load_dataset(dataset_dir, img_size=(img_size, img_size))
+        # Get the training + testing accuracies.
+        print("Calculating accuracies...")
+        model_acc_dirs = calc_accuracies_full(models, train_loader, test_loader, use_cache=args.use_cache, device=device)
 
-    # Split the trainset into train and test.
-    train_set, test_set1 = torch.utils.data.random_split(
-        train_set, [int(len(train_set) * 0.7), int(len(train_set) * 0.3)]
-    )
+        # {model_name: ({epoch: train_accuracy}, {epoch: test_accuracy}})
+        train_accs = {name: accs[0] for name, accs in model_acc_dirs.items()}
+        test_accs = {name: accs[1] for name, accs in model_acc_dirs.items()}
+        train_accs_by_seed[seed] = train_accs
+        test_accs_by_seed[seed] = test_accs
 
-    # Limit the dataset size to make the computations faster.
-    N_train = args.N if args.N else len(train_set)
-    N_val = args.N if args.N else len(test_set1)
-    N_test = args.N if args.N else len(test_set)
-    train_set, _ = torch.utils.data.random_split(train_set, [N_train, len(train_set) - N_train])
-    val_set, _ = torch.utils.data.random_split(test_set1, [N_val, len(test_set1) - N_val])
-    test_set, _ = torch.utils.data.random_split(test_set, [N_test, len(test_set) - N_test])
-    # test_set = val_set      # Pretty sure the original papers "test set" is actually a validation set.
+    # Get the mean + standard error across seeds.
+    train_accs = {}
+    test_accs = {}
+    for seed, train_accs_ in train_accs_by_seed.items():
+        for model_name, accs in train_accs_.items():
+            train_accs.setdefault(model_name, []).append(accs)
+    for seed, test_accs_ in test_accs_by_seed.items():
+        for model_name, accs in test_accs_.items():
+            test_accs.setdefault(model_name, []).append(accs)
 
-    batch_size = 64
-    train_loader = DataLoader(train_set, batch_size=batch_size, num_workers=0)
-    test_loader = DataLoader(test_set, batch_size=batch_size, num_workers=0)
-    
-    # Get the training + testing accuracies.
-    print("Calculating accuracies...")
-    model_acc_dirs = calc_accuracies_full(models, train_loader, test_loader, use_cache=args.use_cache, device=device)
+    train_acc_stats = {}
+    test_acc_stats = {}
+    for model_name, accs in train_accs.items():
+        model_stats = {}
+        for epoch in args.epochs:
+            model_stats[epoch] = (np.mean([acc[epoch] for acc in accs]), np.std([acc[epoch] for acc in accs]) / np.sqrt(len(accs)))
 
-    # {model_name: ({epoch: train_accuracy}, {epoch: test_accuracy}})
-    train_accs = {name: accs[0] for name, accs in model_acc_dirs.items()}
-    test_accs = {name: accs[1] for name, accs in model_acc_dirs.items()}
+        train_acc_stats[model_name] = model_stats
+
+    for model_name, accs in test_accs.items():
+        model_stats = {}
+        for epoch in args.epochs:
+            model_stats[epoch] = (np.mean([acc[epoch] for acc in accs]), np.std([acc[epoch] for acc in accs]) / np.sqrt(len(accs)))
+
+        test_acc_stats[model_name] = model_stats
 
     # Compare the models.
     print("Plotting accuracies...")
-    compare_models(train_accs, test_accs, "./")
+    compare_models(train_acc_stats, test_acc_stats, "./")
 
 
 if __name__ == "__main__":
